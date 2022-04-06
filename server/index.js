c6a15dfbb38c28aa7f509478405447a868d975a3
Matthew Guizar <matthewrguizar@gmail.com>
2022-04-06 11:50:19 -0700

    awkward

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+const PORT = process.env.PORT || 4005
+
+app.listen(PORT, () => { console.log(`listening on ${PORT}`)})
