2bb9429b81407c3c02eaf0e103bc3ae1e18dee65
Matthew Guizar <matthewrguizar@gmail.com>
2022-04-07 13:26:13 -0700

    kinda done

--- public/main.js
+++ public/main.js
@@ -1,8 +1,22 @@
 console.log('fufufufufuooooom')
 
 const contact = document.querySelector('.right');
+const whyBtn = document.querySelector('#why-btn');
+const whoCares = document.querySelector('#why-not')
 
 
-contact.addEventListener('click', () =>{
-    alert('working')
+whyBtn.addEventListener('click', () =>{
+   let box = window.prompt('WHY NOT?')
+    let text;
+    if (box === null || ''){
+       text = ('get in the flask')
+    } else {
+       text = 'alright wise guy GET IN THE FLASK!';
+       window.alert(text)
+
+}
+})
+
+whoCares.addEventListener('click', () =>{
+    alert('dude me too, i didnt even know this thing existed til yesterday')
 })
