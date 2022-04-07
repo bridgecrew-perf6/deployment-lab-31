e279117dbc2001ddf51b7aab99b07f2dfebe7cdf
Matthew Guizar <matthewrguizar@gmail.com>
2022-04-07 11:24:01 -0700

    message

--- server/index.js
+++ server/index.js
@@ -19,6 +19,12 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
+try {
+    nonExistentFunction();
+  } catch (error) {
+    console.error(error);
+}
+
 const PORT = process.env.PORT || 4005
 
 app.listen(PORT, () => { console.log(`listening on ${PORT}`)})
