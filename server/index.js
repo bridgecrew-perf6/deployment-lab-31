b1926410d59975a7edf2a6bf8409f95b94cceeef
Matthew Guizar <matthewrguizar@gmail.com>
2022-04-07 13:49:22 -0700

    update pls

--- server/index.js
+++ server/index.js
@@ -19,9 +19,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join('../index.html', '../purchase'))
-})
 // try {
 //     error();
 //   } catch (error) {
