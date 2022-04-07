bd9edb740697599d324a586b655e1f10d0936ffd
Matthew Guizar <matthewrguizar@gmail.com>
2022-04-07 11:15:17 -0700

    here

--- server/index.js
+++ server/index.js
@@ -4,6 +4,17 @@ const app = express()
 
 app.use(express.static('public'))
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: '79a6013ae83a4ee5b41ff2f1ed6cdafd',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
