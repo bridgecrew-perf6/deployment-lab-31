cc075f391c116ece6bc69ec8dddfc6557f28efb4
Matthew Guizar <matthewrguizar@gmail.com>
2022-04-07 11:28:47 -0700

    message

--- public/main.js
+++ public/main.js
@@ -1,2 +1,11 @@
 console.log('fufufufufuooooom')
 
+const contact = document.querySelector('.contact')
+
+
+const firstEvent = () =>{
+    alert('i dont live there foo')
+}
+
+
+contact.addEventListener('click', firstEvent)
